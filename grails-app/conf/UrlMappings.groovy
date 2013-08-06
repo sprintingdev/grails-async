3c787b405f67f6c04a12ed63508603fe03c2cc32
S Vivek Krishna <svivekkrishna@gmail.com>
2013-08-06 09:15:25 +0530

    Application home to async/index

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,13 @@
 class UrlMappings {
 
 	static mappings = {
+		"/" (controller: 'async', action: 'index')
         "/$controller/$action?/$id?(.${format})?"{
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(view:"/index")
         "500"(view:'/error')
 	}
 }
